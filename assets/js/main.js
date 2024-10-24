4b862e0aea000df1b528eb0a9669030d349fd1be
Luke Olende <olendeluke2k@gmail.com>
2024-10-24 16:27:29 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,9 @@ const swiperHome = new Swiper('.home__swiper', {
 
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
